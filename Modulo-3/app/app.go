d6b1a822fe62d82e32ff75dc1946255b4ef00787
JonnathanRiquelmo <jonnathan.riquelmo@gmail.com>
2022-04-17 03:46:35 -0300

    Update

---
+++ Modulo-3/app/app.go
@@ -0,0 +1,79 @@
+package app
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
+
+// Gerar vai retornar a apliação de linha de comando pronta para ser executada
+func Gerar() *cli.App {
+	app := cli.NewApp()
+	app.Name = "Terminal Application"
+	app.Usage = "Returns IPs and SERVER NAMES on the internet"
+
+	flags := []cli.Flag{
+		//é a definição da flag em um slice, a flag é oq é passado como parametro
+		cli.StringFlag{
+			//nome da flag
+			Name: "host",
+			//valor padrão executado na flag caso nada seja passado no paramêtro
+			Value: "lesse.com.br",
+		},
+	}
+
+	app.Commands = []cli.Command{
+		{
+			//nome do comando
+			Name: "ip",
+			//descrição de uso do comando definição dos parametros que podem ser passados ao comando
+			Usage: "Search ip addresses on the internet",
+			Flags: flags,
+			//execução do comando, define uma ação válida do app
+			Action: searchIps,
+		},
+		// Segundo comando
+		{
+			Name:   "server",
+			Usage:  "Search the server(s) name on the internet",
+			Flags:  flags,
+			Action: searchServers,
+		},
+	}
+
+	return app
+}
+
+func searchIps(c *cli.Context) {
+	//para buscar é necessário salvar o host que está sendo passado
+	host := c.String("host")
+
+	//Pacote net para armazenar os ips em um slice de ips (pq um host pode ter mais de um ip público)
+	ips, erro := net.LookupIP(host)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	fmt.Println("Address(es) found for the host", host)
+	fmt.Println("-------------------------------")
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
+}
+
+func searchServers(c *cli.Context) {
+	host := c.String("host")
+
+	servers, erro := net.LookupNS(host) // Name servers
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	fmt.Println("Server(s) found for the host", host)
+	fmt.Println("-------------------------------")
+	for _, server := range servers {
+		fmt.Println(server.Host)
+	}
+}
---
+++ Modulo-3/readme.md
@@ -0,0 +1,15 @@
+Essa é uma aplicação de terminal feita no terceiro módulo do curso de [**GOLANG**](https://www.udemy.com/course/aprenda-golang-do-zero-desenvolva-uma-aplicacao-completa/) da Udemy.
+---
+A aplicação funciona da seguinte forma:
+
+O comando abaixo retorna o endereço IP de um host passado por paramêtro
+```
+$ go run main.go **ip** --host <endereço>
+```
+
+O comando abaixo retorna o nome do servidor de um host passado por paramêtro
+```
+$ go run main.go **server** --host <endereço>
+```
+
+Os comandos podem ser executados sem os paramêtros de **--host**  **< endereço >**, nesse caso será usado o host padrão (lesse.com.br). 
